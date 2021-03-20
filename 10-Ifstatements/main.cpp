1f8abd662ce973499a75e109fc1571e6b56aa845
Philip Marfo <philcho98@gmail.com>
2021-03-20 20:08:49 +0000

    This talks about if statements

---
+++ 10-Ifstatements/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    bool isMale = false;
+    bool isTall = false;
+
+    if(isMale && isTall){
+        cout<< "You are a tall male";
+    }else if(isMale && !isTall){
+        cout<<"You are a short male";
+
+    }else if(!isMale && isTall){
+        cout<<"You are tall but not male";
+
+    }
+    else{
+        cout<<"You are not male and not tall";
+
+    }
+}
