d7124a80e928754ca26ab5695462b234f43a9f04
Philip Marfo <philcho98@gmail.com>
2021-03-20 20:25:27 +0000

    Using while loops

---
+++ 14-WhileLoops/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  int index = 1;
+
+
+  while(index <=5 ){
+        cout<<index<<endl;
+        index++;
+
+  }
+
+/*
+  do{
+    cout<<index<<endl;
+    index++;
+  }while(index<=5);
+  */
+}
