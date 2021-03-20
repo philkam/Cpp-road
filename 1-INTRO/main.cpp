0bca84f9c347ff1946747f9b423d9622114f7a40
Philip Marfo <philcho98@gmail.com>
2021-03-20 20:05:24 +0000

    This is an intro

---
+++ 1-INTRO/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+//understanding cpp
+    string characterName = "Tom";
+    int age;
+    age = 50;
+
+    cout<< "There once was a man named " <<characterName <<endl;
+    cout<< "He was " <<age << " years old."<<endl;
+
+    characterName = "Mike";
+    cout<< "He liked the name " <<characterName <<endl;
+    cout<< "But did not like being " <<age<<endl;
+
+
+
+
+}
