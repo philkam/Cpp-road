2b764df4ead1cec1796db848a9c0044ee6ca75e1
Philip Marfo <philcho98@gmail.com>
2021-03-20 20:09:22 +0000

    Arrays in C++

---
+++ 7-Arrays/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int luckyNums[] = {4, 8, 15, 16, 23, 42};
+    int luck[20];
+   luckyNums[10] = 100;
+    cout<< luckyNums[0];
+
+
+}
