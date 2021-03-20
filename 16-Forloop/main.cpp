fbc44eecd1ccec0c5585f5c24992cdc42e593bbe
Philip Marfo <philcho98@gmail.com>
2021-03-20 20:10:03 +0000

    Using For Loops in C++

---
+++ 16-Forloop/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    for(int i=1; i<=5;i++){
+    cout<<i<<endl;
+  }
+
+
+  int nums[] = {1, 2, 5, 7, 3};
+  for(int i=0;i<5;i++){
+    cout<<nums[i]<<endl;
+  }
+
+
+}
